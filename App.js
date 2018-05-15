d5b9091e0d6e1e2d8ff9db7189f4eeb22fa1c1c4
choumasamori <5911378@gmail.com>
2018-05-15 16:32:34 +0700

    asd

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import { Container, Header, Content, Item, Input } from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Button, Label} from 'native-base';
 
 class Login extends React.Component {
   render() {
     return (
-      <Container>
+      <Container style={{backgroundColor: 'white'}}>
         <Content>
-          <Item>
-            <Input placeholder="Email" />
-          </Item>
+        <Form>
+            <Item floatingLabel>
+            <Label>Username/Email/Phone Number</Label>
+              <Input/>
+            </Item>
+            <Item floatingLabel>
+            <Label>Password</Label>
+              <Input/>
+            </Item>
+          </Form>
+          <Button rounded primary full onPress={()=>this.props.navigation.navigate('Register')}>
+            <Text style={{color:'white'}}>Log In</Text>
+          </Button>
         </Content>
       </Container>
     );
@@ -27,15 +37,20 @@ class Register extends React.Component {
   }
 }
 
-const RootStack = createStackNavigator(
-  {
-    Login: Login,
-    Register: Register,
+const RootStack = createStackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      title: "Login"
+    }
   },
-  {
-    initialRouteName: 'Login',
+  Register: {
+    screen: Register,
+    navigationOptions: {
+      title: "Register"
+    }
   }
-);
+});
 
 export default class App extends React.Component {
   render() {
